92370321cf7afc1fa9628ff374fe43c2244fab58
geraldkachi <fitzgeraldkachi@gmail.com>
2023-01-29 22:11:55 +0100

    hit

--- src/pages/SelectAssignWorker.tsx
+++ src/pages/SelectAssignWorker.tsx
@@ -27,16 +27,16 @@ const SelectAssignWorker = ({ setStateBool, data }: any) => {
     return (
         <div>
             <div className="flex flex-col max-w-md mx-auto my-auto rounded-md">
-                <div className="">
+                <div className="my-3">
                     <h1 className=" text-[28px] font-semibold">Select Worker to assign song to</h1>
-                    <span className='text-[#888888] '>All Songs uploaded on this platform (3,523)</span>
+                    <span className='text-[#888888]'>All Songs uploaded on this platform (3,523)</span>
                 </div>
                 <form className=" ng-untouched ng-pristine ng-valid" onSubmit={() => null}>
-                    <div className=" flex items-center justify-between w-full">
+                    <div className="flex items-center justify-between w-full">
                         <Input
-                            className="w-full lg:w-96 p-4 pl-10 outline-none text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                            className="w-full flex-1 lg:w-96 p-4 pl-10 outline-none text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
                             divStyle=''
-                            placeholder="Search songs by title or Artiste’s name'"
+                            placeholder="Search songs by title or Artiste&apos;s name"
                             searchIcon
                             type="search"
                             value={search}
