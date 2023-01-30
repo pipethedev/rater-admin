9d666f4e02e20989d49152b75e3ebebeef66060c
geraldkachi <fitzgeraldkachi@gmail.com>
2023-01-30 02:02:11 +0100

    fixedf the loader

--- src/services/authApis.ts
+++ src/services/authApis.ts
@@ -10,7 +10,6 @@ const baseQuery = fetchBaseQuery({
     // baseUrl: process.env.REACT_APP_API_KEY,
     credentials: 'include',
     prepareHeaders: (headers, { getState }: { getState: any }) => {
-        // console.log(getState(), 'getState().auth.token')
         const token = getState().auth?.auth?.token
         if (token) {
             headers.set("AUTHORIZATION", `Bearer ${token}`)
@@ -24,9 +23,7 @@ const baseQuery = fetchBaseQuery({
 const baseQueryWithReauth = async (args: any, api: any, extraOptions: any) => {
     let result = await baseQuery(args, api, extraOptions)
 
-    console.log(result,'result')
     // 401 (Unauthorized) and 403 (Forbidden) status codes
-    // if (result?.error?.originalStatus === 403 || result.error && result.error.status === 401) {
     if ((result?.meta?.response?.status === 403 || result?.error?.status) && result?.meta?.response?.status === 401) {
         console.log('sending refresh token')
         // send refresh token to get new access token
@@ -34,14 +31,12 @@ const baseQueryWithReauth = async (args: any, api: any, extraOptions: any) => {
         // const refreshResult: any = await baseQuery('/refresh', api, extraOptions)
         console.log(refreshResult, "refreshResult")
         if (refreshResult?.data) {
-            // const user = api.getState().auth.user
             const token = api.getState().auth.token
             // store the new token
             api.dispatch(setCredentials({ ...refreshResult.data, token }))
             // retry the original query with new access token
             result = await baseQuery(args, api, extraOptions)
         } else {
-            // api.dispatch(logout())
             toast.error("Something went wrong, Please login again!")
             setTimeout(() => {
                 api.dispatch(logout());
